f67224e5b1688b52c6267ea93ddad19acba206bc
Gabriel-Borba-Oliveira <20241ctb0100026@estudantes.ifpr.edu.br>
2026-06-05 22:33:07 +0000

    Nível B: README com versões e app.html com nome/apresentação

--- portfolio-angular/src/app/app.ts
+++ portfolio-angular/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatCardModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
+  
 })
 export class App {
   protected readonly title = signal('Meu Portfólio');
