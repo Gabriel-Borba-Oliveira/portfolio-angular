551089ae55f07eee58d7f6a6a58959352974e795
Gabriel-Borba-Oliveira <20241ctb0100026@estudantes.ifpr.edu.br>
2026-08-15 05:28:32 +0000

    Aula18: formulario de Contato com POST a API (Reactive Forms + validacao)

--- portfolio-angular/src/app/app.routes.ts
+++ portfolio-angular/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Sobre } from './sobre/sobre';
 import { Projetos } from './projetos/projetos';
-import { Contato } from './contato/contato';
+import { Contato } from './contato';
 import { Catalogo } from './catalogo/catalogo';
 
 export const routes: Routes = [
---
+++ portfolio-angular/src/app/contato.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ContatoService, NovoContato } from './contato.service';
+@Component({
+    selector: 'app-contato',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: './contato.html',
+})
+export class Contato {
+    private fb = inject(FormBuilder);
+    private service = inject(ContatoService);
+    enviando = false; sucesso = ''; erro = '';
+
+    form = this.fb.group({
+        nome: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        mensagem: ['', [Validators.required, Validators.minLength(10)]],
+    });
+
+    onSubmit() {
+        this.sucesso = ''; this.erro = '';
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+        this.enviando = true;
+        this.service.enviar(this.form.getRawValue() as NovoContato).subscribe({
+            next: (resp) => {
+                this.sucesso = resp.mensagem;
+                this.form.reset();
+                this.enviando = false;
+            },
+            error: () => {
+                this.erro = 'Nao foi possivel enviar. Tente novamente.';
+                this.enviando = false;
+            },
+        });
+    }
+}
